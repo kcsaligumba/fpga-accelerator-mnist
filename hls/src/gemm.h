72ee1c83fb689db4ce7391ba5119e66d0842dc4e
Claire <karelclaire123@gmail.com>
2026-04-15 13:07:59 -0500

    added initial hls files

---
+++ hls/src/gemm.h
@@ -0,0 +1,14 @@
+#ifndef GEMM_H
+#define GEMM_H
+
+// Tile dimensions, tuned to fit in BRAM on the xc7z020
+#define TILE_M 32
+#define TILE_K 32
+#define TILE_N 32
+
+// Maximum supported matrix dimension
+#define MAX_DIM 512
+
+void multiply(int *A, int *B, int *C, int M, int K, int N);
+
+#endif
