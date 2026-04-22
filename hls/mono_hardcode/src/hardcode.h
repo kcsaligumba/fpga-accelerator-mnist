c499886c95dd20200010a9b17a2db83a9b40024a
Claire <karelclaire123@gmail.com>
2026-04-22 17:54:51 -0500

    fixed accuracy issue for mono hardcode

--- hls/mono_hardcode/src/hardcode.h
+++ hls/mono_hardcode/src/hardcode.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 
 #define TILE_K  32
-#define TILE_N  64
+#define TILE_N  32
 
 #define FC1_IN  784
 #define FC1_OUT 128
@@ -12,6 +12,10 @@
 #define FC2_OUT  64
 #define FC3_IN   64
 #define FC3_OUT  10
+
+#define REQUANT_SHIFT 31
+#define FC1_M0  1006425
+#define FC2_M0  7466824
 
 
 void mlp(int8_t *A, int32_t *C);
