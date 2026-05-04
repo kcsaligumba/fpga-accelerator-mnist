bbb6aa5d93982bf7f156e0d29395cf78918ddb58
Claire Saligumba <karelclaire123@gmail.com>
2026-05-04 12:33:59 -0500

    renamed mono dyn

---
+++ hls/mono_dynamic/src/dynamic.h
@@ -0,0 +1,30 @@
+#ifndef DYNAMIC_H
+#define DYNAMIC_H
+
+#include <stdint.h>
+
+#define TILE_K    32
+#define TILE_N    32
+#define MAX_BATCH 128
+
+#define FC1_IN  784
+#define FC1_OUT 128
+#define FC2_IN  128
+#define FC2_OUT  64
+#define FC3_IN   64
+#define FC3_OUT  10
+
+#define REQUANT_SHIFT 31
+#define FC1_M0  1006425
+#define FC2_M0  7466824
+
+void mlp(
+    int8_t  *A,
+    int8_t  *W1, int32_t *b1,
+    int8_t  *W2, int32_t *b2,
+    int8_t  *W3, int32_t *b3,
+    int32_t *C,
+    int M
+);
+
+#endif
